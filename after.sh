510982db79d10113a5b0c506bcc43652d1f23783
Lucas Yang <38133356+ycs77@users.noreply.github.com>
2019-12-30 13:14:33 +0800

    Create after.sh

---
+++ after.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# If you would like to do some extra provisioning you may
+# add any commands you wish to this file and they will
+# be run after the Homestead machine is provisioned.
+#
+# If you have user-specific configurations you would like
+# to apply, you may also create user-customizations.sh,
+# which will be run after this script.
+
+# If you're not quite ready for Node 12.x
+# Uncomment these lines to roll back to
+# v11.x or v10.x
+
+# Remove Node.js v12.x:
+#sudo apt-get -y purge nodejs
+#sudo rm -rf /usr/lib/node_modules/npm/lib
+#sudo rm -rf //etc/apt/sources.list.d/nodesource.list
+
+# Install Node.js v11.x
+#curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+#sudo apt-get install -y nodejs
+
+# Install Node.js v10.x
+#curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+#sudo apt-get install -y nodejs
+
+if [ ! -f /usr/local/extra_homestead_software_installed ]; then
+	echo 'installing some extra software'
+
+    # super user
+    sudo -s
+
+    # update php session time
+    SESSION_TIME=86400
+    for i in {2..4} ; do
+        sed -i "s/gc_maxlifetime = 1440/gc_maxlifetime = ${SESSION_TIME}/g" /etc/php/7.${i}/fpm/php.ini
+        service php7.${i}-fpm restart
+    done
+
+    touch /usr/local/extra_homestead_software_installed
+else
+    echo 'extra software already installed'
+fi
